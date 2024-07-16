98d68cc6e4ef2e107d04fcb6a7daae4b0c5f37ae
aakashsaini09 <aakashsaini948585@gmail.com>
2024-07-16 13:13:27 +0530

    updated

--- src/components/CoinInfo.tsx
+++ src/components/CoinInfo.tsx
@@ -3,10 +3,10 @@ import {parse} from "papaparse"
 import { useState } from "react"
 const CoinInfo = () => {
     interface coinInterFace {
-      time: String,
-      rate: String,
-      asset_id_quote: String
-  }
+      time: string,
+      rate: string,
+      asset_id_quote: string
+    }
     const [data, setdata] = useState<coinInterFace[]>([])
     const getAllCoins = async () => {
         const apiKey = import.meta.env.VITE_API_KEY
@@ -18,13 +18,14 @@ const CoinInfo = () => {
         setdata(parseData)
         console.log("data is::", data)
     }
+    
   return (
     <>
     <div className="w-full h-full flex flex-col justify-center items-center">
       <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" onClick={getAllCoins}>Click me</button>
       { data.map((e, i) => {
         return <div key={i} className="flex gap-5">
-          <div className="my-3 border-2 border-black">Time: {e.time}</div>
+          <div className="my-3 border-2 border-black">Time: {new Date(e.time).toLocaleString()}</div>
           <div className="my-3 border-2 border-black">Rate: {e.rate}</div>
           <div className="my-3 border-2 border-black">{e.rate}</div>
         </div>
